92abf53f5c54fdffcf143bc712550605c2290113
YooHeej <sprite1139@gmail.com>
2024-01-08 15:40:52 +0900

    01081540

---
+++ JavaScript/ch07.표준내장 객체/04.String연습.js
@@ -0,0 +1,28 @@
+// 연습 문제
+// 1. 1에서 1000 사이에 0은 몇 번, 1은 몇 번, ..., 9는 몇 번 사용되었는가?
+let numStr = '';
+for (let i = 1; i <= 1000; i++) {
+    numStr += i;
+}
+/* console.log(numStr); */
+
+// 1이 몇 번 사용되었나?
+
+console.log(numStr.replace(/[^1]/g, '').length);
+
+for (let i = 0; i <= 9; i++) {
+let pattern = new RegExp('[^' + i + ']', 'g');
+count = numStr.replace(pattern, '').length;
+console.log(`${i}: ${count}`);
+}
+
+// count 배열을 만들어 일일이 체크
+let countArr = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+for (let i = 0; i < numStr.length; i++) {
+    let num = Number(numStr[i]);
+    countArr[num]++;
+}
+console.log(countArr);
+
+// 2. 디지털 시계, 00:00 ~ 23:59
+// 
